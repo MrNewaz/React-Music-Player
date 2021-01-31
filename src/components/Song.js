32fb81c428bcbc465aa36d6abd46d227474d9454
MrNewaz <saifur.saif@northsouth.edu>
2021-02-01 01:07:56 +0600

    html

---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
+    </div>
+  );
+};
+
+export default Song;

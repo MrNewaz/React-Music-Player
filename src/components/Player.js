10d57326ac16e24fb99c4f095ba3f7e8c8e2afaf
MrNewaz <saifur.saif@northsouth.edu>
2021-02-03 00:12:12 +0600

    slider

--- src/components/Player.js
+++ src/components/Player.js
@@ -75,18 +75,30 @@ const Player = ({
     }
     playAudio(isPlaying, audioRef);
   };
+  //Styles
+  const trackAnim = {
+    transform: `translateX(${songInfo.animatePercentage}%) `,
+  };
 
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input
-          onChange={dragHandler}
-          min={0}
-          max={songInfo.duration || 0}
-          value={songInfo.currentTime}
-          type="range"
-        />
+        <div
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]},${currentSong.color[1]})`,
+          }}
+          className="track"
+        >
+          <input
+            onChange={dragHandler}
+            min={0}
+            max={songInfo.duration || 0}
+            value={songInfo.currentTime}
+            type="range"
+          />
+          <div style={trackAnim} className="animate-track"></div>
+        </div>
         <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
       </div>
       <div className="play-control">

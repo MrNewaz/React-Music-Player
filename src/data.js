d7a1bf1ff2466cfa37d85889c588c36949e0b378
MrNewaz <saifur.saif@northsouth.edu>
2021-02-03 23:17:23 +0600

    fix

--- src/data.js
+++ src/data.js
@@ -3,6 +3,26 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
+      name: "Deyale Deyale",
+      cover: "https://m.media-amazon.com/images/I/81OAokRhjPL._SS500_.jpg",
+
+      artist: "Minar",
+      id: uuidv4(),
+      color: ["#801B21", "#F6E89B"],
+      audio: "https://sumirbd.mobi/files/download/id/70272",
+      active: false,
+    },
+    {
+      name: "Smile",
+      cover: "https://i.scdn.co/image/ab67616d0000b273b51c32c3c6cabcfe016c08a4",
+
+      artist: "Uncle Kracker",
+      id: uuidv4(),
+      color: ["#801B21", "#F6E89B"],
+      audio: "https://download.my-free-music.icu/o/Uncle-Kracker-Smile.mp3",
+      active: false,
+    },
+    {
       name: "Beaver Creek",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
@@ -72,7 +92,7 @@ function chillHop() {
       color: ["#BA4A46", "#FDF0DD"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9363",
       id: uuidv4(),
-      active: true,
+      active: false,
     },
     {
       name: "Lagoons",
@@ -82,26 +102,6 @@ function chillHop() {
       id: uuidv4(),
       color: ["#BA4A46", "#FDF0DD"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8266",
-      active: false,
-    },
-    {
-      name: "Deyale Deyale",
-      cover: "https://m.media-amazon.com/images/I/81OAokRhjPL._SS500_.jpg",
-
-      artist: "Minar",
-      id: uuidv4(),
-      color: ["#801B21", "#F6E89B"],
-      audio: "https://sumirbd.mobi/files/download/id/70272",
-      active: false,
-    },
-    {
-      name: "Smile",
-      cover: "https://i.scdn.co/image/ab67616d0000b273b51c32c3c6cabcfe016c08a4",
-
-      artist: "Uncle Kracker",
-      id: uuidv4(),
-      color: ["#801B21", "#F6E89B"],
-      audio: "https://download.my-free-music.icu/o/Uncle-Kracker-Smile.mp3",
       active: false,
     },
   ];
